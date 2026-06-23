2191ab30393907c49e55f76d07c1d9baa2aa673b
DiegoBeltran-B <barajasfernando08@gmail.com>
2026-06-23 09:46:15 -0500

    se agrego la constante cors y se agrego la condicional en app.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const router = require('./routes/router')
-// const cors = require('cors')
-// app.use(cors())
+const cors = require('cors')
+app.use(cors())
 app.use('/api', router)
 app.listen (4000, () =>{
     console.log('cree un servidor web')
